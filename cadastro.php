195f849e945f704114c46ded7db75ae466029794
Wanderson Machado <wanmachadojr@gmail.com>
2021-03-19 13:30:15 -0300

    Wanderson Machado
    Front-end

---
+++ cadastro.php
@@ -0,0 +1,27 @@
+<html lang ="pt-br">
+<head>
+<meta charset="UTF-8">
+	<title> Community leadership </title>
+    <link rel= "stylesheet" href="css/style.css">
+</head>
+<body>
+
+
+
+    <div id="corpo-form">
+	<h1> Cadastra-se </h1>
+     <form method= "POST">
+         <input type="text"         name="nome"         placeholder="Nome Completo" maxlength="30">
+         <input type="text"         name="telefone"     placeholder="Telefone" maxlength="30">
+         <input type="email"        name="email"        placeholder="Usuario" maxlength="40">
+         <input type="password"     name="senha"        placeholder="Senha" maxlength="15">
+         <input type="password"     name="confSenha"    placeholder="Confirmar Senha" maxlength="15">
+         <input type="submit"       value="Cadastrar">
+         <a href="index.php" > Voltar e <strong>Logar</strong>
+     </form>
+     </div>
+
+     
+
+     </body>
+</html>
---
+++ index.php
@@ -0,0 +1,22 @@
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel= "stylesheet" href="css/style.css">
+    <title> Community leadership </title>
+</head>
+<body>
+
+
+<div id="corpo-form">
+	<h1>  Entrar <br> </h1>
+     <form method= "POST">
+         <input type="email"      placeholder="Usuario"   name="email">
+         <input type="password"   placeholder="Senha"     name="senha">
+         <input type="submit"     value="Login">
+         <a href="cadastro.php" > Ainda não é inscrito? <strong>Cadastre-se</strong>
+     </form>
+     </div>
+
+      
+</body>
+</html>
